6acb97e3de36282851dbfe71e8a725eea48cbcb3
Torido <nhu.truong213@gmail.com>
2025-08-06 12:46:12 +0700

    feat: add TexboxPage and TextboxTest

--- src/test/java/tests/TextboxTest.java
+++ src/test/java/tests/TextboxTest.java
@@ -1,4 +1,48 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.elements.TextboxPage;
+import utils.CommonActions;
+
+import static utils.Browsers.openBrowser;
+import static utils.CommonActions.driver;
+
 public class TextboxTest {
+
+    //1. Mở https://demoqa.com/text-box
+    //2. Nhập Full Name, Email, Address
+    //3. Nhấn Submit
+    TextboxPage textboxPage;
+
+    @BeforeMethod
+    public void setup() {
+        openBrowser("chrome");
+        textboxPage = new TextboxPage();
+        textboxPage.open();
+
+
+    }
+
+    @DataProvider(name = "fullData1")
+    public Object [][] fullData() {
+        return new Object[][]{
+                {"Tran Thuy Vy", "thuyvy@gmail.com", "123 Le Loi, HCM", "456 Le Loi, HCM"},
+                {"Truong Van Dom", "truongdom@gmail.com", "789 Le Loi, HCM", "101112 Le Loi, HCM"}
+        };
+    }
+
+    @Test (dataProvider = "fullData1")
+    public void verifyInformationSubmission(String fullName, String email, String currentAddress, String permanentAddress) {
+        textboxPage.fillForm(fullName, email, currentAddress, permanentAddress);
+        textboxPage.clickSubmit();
+        Assert.assertEquals(textboxPage.getFullName(), "Name:" + fullName, "Full Name does not match");
+    }
+
+
+
 }
